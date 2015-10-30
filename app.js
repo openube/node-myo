f917d12524e77793b0eac527b2fc91c14e933b4d
Ronnie Chong <ronnie@otakuness.com>
2015-10-30 22:14:43 +1100

    More efficient

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ var pebble          = require('./components/pebble'),
     drone           = require('./components/drone'),
     keypress        = require('./components/keypress'),
     port            = 1337,
-    myDrone,
-    myMyo,
-    myTunnel;
+    myTunnel        = localtunnel(port, 'somesubdomainabc123'),
+    myDrone         = drone.connect(),
+    myMyo           = myo.connect(myDrone);
 
-myTunnel = localtunnel(port, 'somesubdomainabc123');
-myDrone = drone.connect();
-myMyo = myo.connect(myDrone);
 pebble.setServer(port,myDrone);
 keypress.setListeners(myDrone,myMyo,myTunnel);
 
