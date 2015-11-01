587b6c2d7c02cf933a98dc1a36403dd20ad08364
Ronnie Chong <ronnie@otakuness.com>
2015-11-01 11:59:12 +1100

    Pebble and server should be separate

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var pebble          = require('./components/pebble'),
+var server          = require('./components/server'),
+    pebble          = require('./components/pebble'),
     localtunnel     = require('./components/localtunnel'),
     myo             = require('./components/myo'),
     drone           = require('./components/drone'),
     keypress        = require('./components/keypress'),
+    socketio        = require('./components/socketio'),
+
     port            = 1337,
+    pebbleUrl       = '/pebblecall',
     myTunnel        = localtunnel(port, 'somesubdomainabc123'),
     myDrone         = drone.connect(),
-    myMyo           = myo.connect(myDrone);
+    myMyo           = myo.connect(myDrone),
+    myApp           = server.setServer(port);
 
-pebble.setServer(port,myDrone);
-keypress.setListeners(myDrone,myMyo,myTunnel);
-
+keypress.setListeners(myDrone,myMyo, myTunnel);
+pebble.setAjaxCall(myApp.app, myDrone, pebbleUrl);
--- components/pebble.js
+++ components/pebble.js
@@ -1,57 +1,31 @@
-var app             = require('express')(),
-    bodyParser      = require('body-parser'),
-    http            = require('http').Server(app);
-
 module.exports = {
-    setServer : function(port, drone){
+  setAjaxCall: function(app,drone,url){
+      app.post(url, function (req, res) {
+          var a = req.body,
+              response = {};
 
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}));
-
-        app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
-
-        app.get('/', function (req, res) {
-            res.setHeader('Content-Type', 'text');
-            res.send('Nothing here');
-
-        });
-
-        app.post('/pebblecall', function (req, res) {
-            var a = req.body,
-                response = {};
-
-            switch (a.type) {
-                case 'ping':
-                    console.log('Got a ping from pebble');
-                    response.text = 'Success';
-                    response.type = a.type;
-                    break;
-                case 'takeoff':
-                case 'land':
-                case 'emergency':
-                case 'flipfront':
-                case 'flipback':
-                case 'flipleft':
-                case 'flipright':
-                    drone.setRollingSpider(a.type);
-                    response.type = a.type;
-                    break;
-                default :
-                    response.text = 'Nothing to see here';
-                    break;
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(response));
-        });
-
-        app.listen(port, function () {
-            console.log('Running server at port %s', port);
-        });
-
-    }
+          switch (a.type) {
+              case 'ping':
+                  console.log('Got a ping from pebble');
+                  response.text = 'Success';
+                  response.type = a.type;
+                  break;
+              case 'takeoff':
+              case 'land':
+              case 'emergency':
+              case 'flipfront':
+              case 'flipback':
+              case 'flipleft':
+              case 'flipright':
+                  drone.setRollingSpider(a.type);
+                  response.type = a.type;
+                  break;
+              default :
+                  response.text = 'Nothing to see here';
+                  break;
+          }
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify(response));
+      });
+  }
 };
