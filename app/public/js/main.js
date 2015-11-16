90a601a97dae82d5a39ef46fe6d25ede578858d1
Ronnie Chong <ronnie@otakuness.com>
2015-11-17 07:49:37 +1100

    Added UI and sass

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,10 +1,6 @@
-//var socket = io.connect('http://localhost:1337');
-//
-//socket.on('connect', function(data) {
-//    console.log('connected');
-//    socket.emit('success', 'Success');
-//});
-//
-//socket.on('website', function(data){
-//   console.log('Received from server =', data);
-//});
+var socket = io.connect('http://localhost:1337');
+
+socket.on('connect', function(data) {
+    console.log('connected');
+    socket.emit('success', 'Success');
+});
