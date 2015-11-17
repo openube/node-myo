bbad9974926414837608b944cf755709874c4183
Ronnie Chong <ronnie@otakuness.com>
2015-11-18 08:25:27 +1100

    House keeping

--- app/components/pebble.js
+++ app/components/pebble.js
@@ -1,7 +1,10 @@
+var url, io, drone;
+
 module.exports = {
-  setAjaxCall: function(app,drone,settings){
-      var url   = settings.config.pebbleUrl,
-          io    = settings.module.socketio;
+  setAjaxCall: function(app,_drone,settings){
+      url   = settings.config.pebbleUrl;
+      io    = settings.module.socketio;
+      drone = _drone;
 
       app.post(url, function (req, res) {
           var a = req.body,
@@ -9,13 +12,9 @@ module.exports = {
 
           switch (a.type) {
               case 'ping':
-                  console.log('Pebble: Got a ping from pebble');
-                  //io.emit('web',{
-                  //    'type': ,
-                  //    'data': ''
-                  //});
                   response.text = 'Success';
                   response.type = a.type;
+                  console.log('Pebble: Got a ping from pebble');
                   io.emit('console',{'data': 'Pebble: Received a ping'});
                   break;
               case 'takeoff':
@@ -27,6 +26,7 @@ module.exports = {
               case 'flipright':
                   drone.setRollingSpider(a.type);
                   response.type = a.type;
+
                   console.log('Pebble: Received', a.type);
                   io.emit('console',{'data': 'Pebble: Received ' + a.type});
                   break;
